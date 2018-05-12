efb85793e9791192e2d2b617fd2e399ec283eb72
Jaume GARCIA <jagarcia@e1r8p10.42.fr>
2018-05-13 00:07:07 +0200

    modificados strdel y strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: jagarcia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/07 11:55:57 by jagarcia          #+#    #+#             */
-/*   Updated: 2017/11/11 02:32:32 by jagarcia         ###   ########.fr       */
+/*   Updated: 2018/05/12 20:53:55 by mrodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,18 @@
 char	*ft_strdup(const char *src)
 {
 	char	*str1;
-	char	*srccpy;
 	int		len;
 	int		i;
 
-	srccpy = (char *)src;
-	len = ft_strlen(srccpy);
-	str1 = (char *)malloc(sizeof(*str1) * (len + 1));
-	if (str1 == NULL)
+	if (!src)
+		return (NULL);
+	len = ft_strlen(src);
+	if (!(str1 = (char *)malloc(sizeof(*str1) * (len + 1))))
 		return (NULL);
 	i = 0;
-	while (srccpy[i])
+	while (src[i])
 	{
-		str1[i] = srccpy[i];
+		str1[i] = src[i];
 		i++;
 	}
 	str1[i] = '\0';
