7b8c99dfb54bec5800e15adf8d4072c7536f1c97
Jaume <carterillo@hotmail.com>
2020-04-17 05:41:58 +0200

    improved makefile, cleaned files, removed printf

--- includes/libftprintf.h
+++ includes/libftprintf.h
@@ -22,16 +22,13 @@
 # include "utils.h"
 
 static const char	g_types[25] = {"%sScCidDoOuUxXpbrfFeEgGnm"};
-static int			(*g_type_func[25])(char *, va_list *, char **, size_t len) =
-{ft_percent_type, ft_sls_type, ft_sls_type, ft_clc_type, ft_clc_type,
-	ft_ilidld_type, ft_ilidld_type,
-	ft_ilidld_type, ft_oloulu_type, ft_oloulu_type, ft_oloulu_type,
-	ft_oloulu_type, ft_xlx_type, ft_xlx_type, ft_p_type, ft_b_type,
-	ft_r_type, ft_feleglg_type, ft_feleglg_type, ft_feleglg_type,
-	ft_feleglg_type, ft_feleglg_type, ft_feleglg_type, ft_n_type, ft_m_type};
+extern int			(*g_type_func[25])(char *, va_list *, char **, size_t len);
+
 static const char	g_mods[6][3] = {"h", "hh", "ll", "l", "z", "j"};
-static const char	*g_format = "-+ 0'#*.";
-static char			*(*g_mod_selector[7])(va_list, va_list, char *) =
+extern const char	*g_format;
+const char	*g_format = "-+ 0'#*.";
+extern char			*(*g_mod_selector[7])(va_list, va_list, char *);
+char			*(*g_mod_selector[7])(va_list, va_list, char *) =
 {ft_none_mod, ft_h_mod, ft_hh_mod, ft_ll_mod, ft_l_mod, ft_z_mod, ft_j_mod};
 int					ft_printf(const char *str, ...);
 int					ft_asprintf(char **buffer, const char *str, ...);
